6092cf04d6161856e95922e21a251a6e52e58a35
pushpak01 <70345937+pushpak01@users.noreply.github.com>
2025-09-20 17:51:15 +0530

    Update wsgi.py

--- portfolio_pushpak/wsgi.py
+++ portfolio_pushpak/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio-pushpak.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_pushpak.settings')
 application = get_wsgi_application()
